57aa228bf523a6a7f606431479fdd8b79f8b9c6b
Naoki Masuda <info@link-mkt.work>
2020-02-01 14:55:26 +0900

    first commit

---
+++ study1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>ファイル1</title>
+</head>
+<body>
+	<p>ファイル1の1回目です。</p>
+</body>
+</html>
---
+++ study2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>ファイル2</title>
+</head>
+<body>
+	<p>ファイル2の1回目です。</p>
+</body>
+</html>
